546a6739c851d675f5821059804f8330cc6ced5c
felix.Wang <felix@thinkingdata.cn>
2021-06-28 15:09:40 +0800

    optimize message format

--- dolphinscheduler-alert/src/main/java/org/apache/dolphinscheduler/alert/utils/Constants.java
+++ dolphinscheduler-alert/src/main/java/org/apache/dolphinscheduler/alert/utils/Constants.java
@@ -133,7 +133,7 @@ public class Constants {
 
     public static final int ALERT_SCAN_INTERVAL = 5000;
 
-    public static final String MARKDOWN_QUOTE = ">";
+    public static final String MARKDOWN_QUOTE = "  ";
 
     public static final String MARKDOWN_ENTER = "\n";
 
